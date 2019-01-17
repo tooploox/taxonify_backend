ac58078ed5cd4244dd7d8bc5969b0e436faf8d30
Olek Cieslak <olek.cieslak@yahoo.com>
2019-01-17 21:35:56 +0100

    Added dummy MongoDB integration

--- aquascope/webserver/app.py
+++ aquascope/webserver/app.py
@@ -1,7 +1,15 @@
-from flask import Flask, request
+import datetime
+import os
 import logging
 
+from flask import Flask, request
+from pymongo import MongoClient
+
 from aquascope.tasks.celery import celery_app
+
+mongo_connection_string = os.environ['MONGO_CONNECTION_STRING']
+mongo_client = MongoClient(mongo_connection_string)
+db = mongo_client.get_database()
 
 logging.basicConfig(filename='webserver.log', level=logging.DEBUG,
                     format="[%(asctime)s] %(levelname)s in %(module)s: %(message)s")
@@ -17,7 +25,12 @@ def after_request(response):
 
 @app.route('/')
 def mainpoint():
-    return "Hello world."
+    post = {"author": "Mike",
+            "text": "My first blog post!",
+            "tags": ["mongodb", "python", "pymongo"],
+            "date": datetime.datetime.utcnow()}
+    id = db.posts.insert_one(post).inserted_id
+    return f'Hello world {id}'
 
 
 @app.route('/task')
@@ -27,3 +40,7 @@ def task():
     celery_app.send_task('aquascope.tasks.add.task',
                          args=[5, 3])
     return "I'm about to schedule a task."
+
+
+if __name__ == "__main__":
+    app.run()
