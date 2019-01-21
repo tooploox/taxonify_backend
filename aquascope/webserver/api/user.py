a1b9afdb73c6f423e3514986bf72038a357f5f51
Olek Cieslak <olek.cieslak@yahoo.com>
2019-01-21 15:34:03 +0100

    Initial scaffold for Swagger

--- aquascope/webserver/api/user.py
+++ aquascope/webserver/api/user.py
@@ -1,6 +1,6 @@
 from passlib.hash import pbkdf2_sha256 as sha256
 
-from flask import current_app as app
+from flask import current_app as app, request
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import (create_access_token, create_refresh_token,
                                 jwt_refresh_token_required, get_jwt_identity)
@@ -8,10 +8,7 @@ from flask_jwt_extended import (create_access_token, create_refresh_token,
 
 class UserLogin(Resource):
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('username', help='This field is mandatory', required=True)
-        parser.add_argument('password', help='This field is mandatory', required=True)
-        data = parser.parse_args()
+        data = request.get_json(force=True)
 
         try:
             verified_user = sha256.verify(data['password'],
