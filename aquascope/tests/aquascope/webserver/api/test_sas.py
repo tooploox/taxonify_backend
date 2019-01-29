1da1e0bfc274a6bcb16f68251801aafa00fbcbb3
Olek Cieslak <olek.cieslak@yahoo.com>
2019-01-29 11:55:48 +0100

    Added /sas GET API test for additional bad argument in request

--- aquascope/tests/aquascope/webserver/api/test_sas.py
+++ aquascope/tests/aquascope/webserver/api/test_sas.py
@@ -46,6 +46,20 @@ class TestGetSas(FlaskAppTestCase):
             res = self.client().get('/sas', query_string=request_data, headers=self.headers)
             self.assertEqual(res.status_code, 400)
 
+    @mock.patch('aquascope.webserver.data_access.storage.blob.generate_container_download_sas')
+    def test_api_can_get_sas_token_for_request_with_bad_argument(self, mock_sas):
+        mocked_sas = 'mockedsas'
+        mock_sas.return_value = mocked_sas
+        valid_destinations = tuple(GROUP_ID_TO_CONTAINER.keys())
+
+        with self.app.app_context():
+            request_data = {
+                'destination': valid_destinations[0],
+                'dummy': 'value'
+            }
+            res = self.client().get('/sas', query_string=request_data, headers=self.headers)
+            self.assertEqual(res.status_code, 400)
+
 
 if __name__ == '__main__':
     unittest.main()
