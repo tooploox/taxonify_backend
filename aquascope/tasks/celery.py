4ef6e123ba6f65aec9820d416f1ea53f236ffb23
Olek Cieslak <olek.cieslak@yahoo.com>
2019-02-11 11:51:56 +0100

    New upload handle implemented

--- aquascope/tasks/celery.py
+++ aquascope/tasks/celery.py
@@ -5,4 +5,5 @@ def make_celery_app(user, password, address):
     return Celery('aquascope',
                   backend='rpc://',
                   broker=f'pyamqp://{user}:{password}@{address}',
-                  include=['aquascope.tasks.add'])
+                  include=['aquascope.tasks.add',
+                           'aquascope.tasks.upload_postprocess'])
--- aquascope/webserver/api/upload.py
+++ aquascope/webserver/api/upload.py
@@ -2,6 +2,7 @@ from flask import current_app as app, request
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 
+import aquascope.webserver.data_access.db.upload as upload
 import aquascope.webserver.data_access.storage.blob as blob
 
 
@@ -14,4 +15,17 @@ class Upload(Resource):
         if not blob.exists(storage_client, container_name):
             blob.create_container(storage_client, container_name)
 
-        blob.create_blob_from_stream(storage_client, container_name, filename, request.stream)
+        db = app.config['db']
+        upload_doc = upload.create(db, filename)
+        blob_filename = str(upload_doc.inserted_id)
+        blob_meta = dict(filename=filename)
+        blob.create_blob_from_stream(storage_client, container_name, blob_filename, request.stream,
+                                     blob_meta)
+
+        celery_app = app.config['celery']
+        celery_app.send_task('aquascope.tasks.upload_postprocess.parse_upload',
+                             args=[blob_filename])
+
+        return {
+            'message': 'Ok'
+        }
