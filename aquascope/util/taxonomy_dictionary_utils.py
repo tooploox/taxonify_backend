fc0644d74dccbc87d5141b3073615725d600e961
Michal Koza <michal.koza@microscopeit.com>
2019-02-15 06:50:02 +0100

    AQ-124 map some species values to Not Specified
    Also add capitalization and trailing spaces removal

--- aquascope/util/taxonomy_dictionary_utils.py
+++ aquascope/util/taxonomy_dictionary_utils.py
@@ -1,3 +1,7 @@
+strings_to_replace_with_not_specified = ["Sp", "Sp."]
+not_specified_string = "Not specified"
+
+
 def add(node, values):
     """Add values as consecutive levels to the node dictionary
     node -- a dictionary
@@ -6,8 +10,11 @@ def add(node, values):
     if values:
         value = values[0]
         if isinstance(value, str):
-            if value not in node:
-                node[value] = {}
+            capitalized_value = value.capitalize().strip()
+            if capitalized_value in strings_to_replace_with_not_specified:
+                capitalized_value = not_specified_string
+            if capitalized_value not in node:
+                node[capitalized_value] = {}
             rest = values[1:]
             if rest:
-                add(node[value], rest)
+                add(node[capitalized_value], rest)
