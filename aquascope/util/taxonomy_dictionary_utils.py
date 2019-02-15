2075b74554a5098667e2d65b988f83075a494a56
Michal Koza <michal.koza@microscopeit.com>
2019-02-15 07:12:26 +0100

    AQ-124 remove some species values as Not Specified

--- aquascope/util/taxonomy_dictionary_utils.py
+++ aquascope/util/taxonomy_dictionary_utils.py
@@ -1,5 +1,4 @@
-strings_to_replace_with_not_specified = ["Sp", "Sp."]
-not_specified_string = "Not specified"
+strings_to_remove = ["Sp", "Sp."]
 
 
 def add(node, values):
@@ -11,10 +10,9 @@ def add(node, values):
         value = values[0]
         if isinstance(value, str):
             capitalized_value = value.capitalize().strip()
-            if capitalized_value in strings_to_replace_with_not_specified:
-                capitalized_value = not_specified_string
-            if capitalized_value not in node:
-                node[capitalized_value] = {}
-            rest = values[1:]
-            if rest:
-                add(node[capitalized_value], rest)
+            if capitalized_value not in strings_to_remove:
+                if capitalized_value not in node:
+                    node[capitalized_value] = {}
+                rest = values[1:]
+                if rest:
+                    add(node[capitalized_value], rest)
