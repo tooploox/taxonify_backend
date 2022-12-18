6fd9cab8a68a5df574538772f4a6cac1c6d28a0b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-18 07:12:07 +0000

    Adding tarfile member sanitization to extractall()

--- aquascope/data_processing/upload_postprocess.py
+++ aquascope/data_processing/upload_postprocess.py
@@ -15,7 +15,26 @@ def download_and_extract_upload(upload_id, container_name, download_path, extrac
     blob.download_blob(storage_client, container_name, upload_id, download_path)
 
     with tarfile.open(download_path, "r") as tar:
-        tar.extractall(extraction_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, extraction_path)
 
 
 def extraction_path_to_data_path(extr_path):
